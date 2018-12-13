f54df672b7617678cd522faefbf039fffd94a706
Christian Vasquez <christianvasquezmelo@gmail.com>
2018-12-13 16:36:07 -0400

    Prevent crash for empty fields
    The app would crash due to a NumberFormatException when any of the
    TextInputEditText's content would be empty, this is now fixed by
    returning 0 in case the field is empty.
    
    Resolves: #15

--- app/src/main/java/com/chrisvasqm/cuadramo/extensions/EditableExtensions.kt
+++ app/src/main/java/com/chrisvasqm/cuadramo/extensions/EditableExtensions.kt
@@ -2,4 +2,6 @@ package com.chrisvasqm.cuadramo.extensions
 
 import android.text.Editable
 
-fun Editable?.toInt(): Int = this.toString().toInt()
+fun Editable?.toInt(): Int =
+        if (this.toString().isNotBlank()) this.toString().toInt()
+        else 0
