afa6545a5c83d1dee60b10196861d3fa269eebdf
Christian Vasquez <christianvasquezmelo@gmail.com>
2018-12-18 12:44:17 -0400

    Display documents worked

--- app/src/main/java/com/chrisvasqm/cuadramo/ui/PreviewBottomSheetDialogFragment.kt
+++ app/src/main/java/com/chrisvasqm/cuadramo/ui/PreviewBottomSheetDialogFragment.kt
@@ -5,29 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.fragment.app.FragmentManager
 import com.chrisvasqm.cuadramo.R
 import com.chrisvasqm.cuadramo.data.models.Cuadre
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class PreviewBottomSheetDialogFragment : BottomSheetDialogFragment() {
 
-    private var cuadre: Cuadre? = null
+    private var cuadre = Cuadre()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.bottom_dialog_cuadre_preview, container, false)
 
         val textCash = view?.findViewById<TextView>(R.id.textCash)
-        textCash?.text = cuadre?.cash.toString()
+        textCash?.text = cuadre.cash.toString()
+
+        val textDocuments = view?.findViewById<TextView>(R.id.textDocuments)
+        textDocuments?.text = (cuadre.ticketsTotal - cuadre.ticketsLeft).toString()
 
         return view
-    }
-
-    override fun show(manager: FragmentManager?, tag: String?) {
-        if (this.cuadre == null)
-            throw IllegalArgumentException("Cuadre must not be null.")
-
-        super.show(manager, tag)
     }
 
     fun setCuadre(cuadre: Cuadre) {
