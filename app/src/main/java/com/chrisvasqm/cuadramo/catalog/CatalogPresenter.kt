7d3c9a0409974b616885d8dca7fb6a74402a3d8b
Christian Vasquez <christianvasquezmelo@gmail.com>
2018-12-21 11:17:27 -0400

    Display cuadres

--- app/src/main/java/com/chrisvasqm/cuadramo/catalog/CatalogPresenter.kt
+++ app/src/main/java/com/chrisvasqm/cuadramo/catalog/CatalogPresenter.kt
@@ -1,6 +1,11 @@
 package com.chrisvasqm.cuadramo.catalog
 
 import com.chrisvasqm.cuadramo.data.models.Cuadre
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class CatalogPresenter : CatalogContract.Presenter {
 
@@ -15,26 +20,26 @@ class CatalogPresenter : CatalogContract.Presenter {
     }
 
     override fun loadCatalog() {
-        view?.showCatalog(mutableListOf(
-                Cuadre(
-                        300,
-                        10,
-                        2,
-                        100,
-                        2,
-                        150,
-                        50
-                ),
-                Cuadre(
-                        300,
-                        10,
-                        2,
-                        100,
-                        2,
-                        150,
-                        50
-                )
-        ))
+        val database = FirebaseDatabase.getInstance()
+        val auth = FirebaseAuth.getInstance()
+        val userId = auth.currentUser?.uid
+        val cuadresReference = database.getReference("users/$userId/cuadres")
+        val cuadres = mutableListOf<Cuadre>()
+        cuadresReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                cuadres.clear()
+                snapshot.children.forEach {
+                    val cuadre = it.getValue(Cuadre::class.java)
+                    cuadre?.let { c -> cuadres.add(c) }
+                }
+
+                view?.showCatalog(cuadres)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
     }
 
     override fun signOut() {
