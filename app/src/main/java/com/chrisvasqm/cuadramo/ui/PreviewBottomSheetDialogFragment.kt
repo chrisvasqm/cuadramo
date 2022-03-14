e793a70cbc4640862d93634165a58210d75a5f7b
chrisvasqm <christianvasquezmelo@gmail.com>
2022-03-13 23:19:37 -0400

    Cleanup code

--- app/src/main/java/com/chrisvasqm/cuadramo/ui/PreviewBottomSheetDialogFragment.kt
+++ app/src/main/java/com/chrisvasqm/cuadramo/ui/PreviewBottomSheetDialogFragment.kt
@@ -4,10 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.FragmentManager
 import com.chrisvasqm.cuadramo.R
 import com.chrisvasqm.cuadramo.data.models.Cuadre
+import com.chrisvasqm.cuadramo.databinding.BottomDialogCuadrePreviewBinding
 import com.chrisvasqm.cuadramo.extensions.toString
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.button.MaterialButton
@@ -16,32 +16,28 @@ import com.google.firebase.database.FirebaseDatabase
 
 class PreviewBottomSheetDialogFragment : BottomSheetDialogFragment() {
 
+    private lateinit var binding: BottomDialogCuadrePreviewBinding
+
     private var cuadre = Cuadre()
 
     private lateinit var manager: FragmentManager
 
     var isPreview = false
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.bottom_dialog_cuadre_preview, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = BottomDialogCuadrePreviewBinding.inflate(inflater, container, false)
 
-        val textDate = view?.findViewById<TextView>(R.id.textDate)
-        textDate?.text = cuadre.createdAt.toString("yyyy/MM/dd HH:mm:ss")
+        binding.textDate.text = cuadre.createdAt.toString("yyyy/MM/dd HH:mm:ss")
+        binding.textCash.text = cuadre.cash.toString()
+        binding.textDocuments.text = (cuadre.ticketsSold).toString()
+        binding.textExpenses.text = cuadre.expenses.toString()
+        binding.textResult.text = cuadre.revenue.toString()
 
-        val textCash = view?.findViewById<TextView>(R.id.textCash)
-        textCash?.text = cuadre.cash.toString()
-
-        val textDocuments = view?.findViewById<TextView>(R.id.textDocuments)
-        textDocuments?.text = (cuadre.ticketsSold).toString()
-
-        val textExpenses = view?.findViewById<TextView>(R.id.textMoneyOff)
-        textExpenses?.text = cuadre.expenses.toString()
-
-        val textResult = view?.findViewById<TextView>(R.id.textResult)
-        textResult?.text = cuadre.revenue.toString()
-
-        val btnSave = view?.findViewById<MaterialButton>(R.id.btnSave)
-        btnSave?.setOnClickListener {
+        binding.btnSave.setOnClickListener {
             val database = FirebaseDatabase.getInstance()
             val reference = database.reference
             val key = reference.child("games").push().key
@@ -49,12 +45,11 @@ class PreviewBottomSheetDialogFragment : BottomSheetDialogFragment() {
             val auth = FirebaseAuth.getInstance()
             val userId = auth.currentUser?.uid
             if (userId != null) {
-                reference
-                        .child("users")
-                        .child(userId)
-                        .child("cuadres")
-                        .push()
-                        .setValue(cuadre)
+                reference.child("users")
+                    .child(userId)
+                    .child("cuadres")
+                    .push()
+                    .setValue(cuadre)
             }
 
             this.dismiss()
@@ -63,10 +58,10 @@ class PreviewBottomSheetDialogFragment : BottomSheetDialogFragment() {
 
         // Hide the save button when the user is just checking an existing cuadre.
         if (isPreview) {
-            btnSave?.visibility = View.GONE
+            binding.btnSave.visibility = View.GONE
         }
 
-        return view
+        return binding.root
     }
 
     fun setCuadre(cuadre: Cuadre) {
