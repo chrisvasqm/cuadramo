489e33d5b0a44fb373c7e82f76eb27fd11f18bf2
chrisvasqm <christianvasquezmelo@gmail.com>
2022-03-14 17:32:01 -0400

    Remove MVP from CatalogActivity.kt

--- app/src/main/java/com/chrisvasqm/cuadramo/catalog/CatalogActivity.kt
+++ app/src/main/java/com/chrisvasqm/cuadramo/catalog/CatalogActivity.kt
@@ -1,6 +1,8 @@
 package com.chrisvasqm.cuadramo.catalog
 
 import android.content.DialogInterface
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -11,25 +13,24 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.chrisvasqm.cuadramo.R
+import com.chrisvasqm.cuadramo.about.AboutActivity
 import com.chrisvasqm.cuadramo.data.models.Cuadre
 import com.chrisvasqm.cuadramo.databinding.ActivityCatalogBinding
+import com.chrisvasqm.cuadramo.editor.EditorActivity
+import com.chrisvasqm.cuadramo.signin.SignInActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.toolbar.*
 
-class CatalogActivity : AppCompatActivity(), CatalogContract.View {
+class CatalogActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCatalogBinding
 
     private lateinit var auth: FirebaseAuth
 
     private lateinit var client: GoogleSignInClient
-
-    private lateinit var presenter: CatalogContract.Presenter
-
-    private lateinit var router: CatalogContract.Router
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,8 +39,7 @@ class CatalogActivity : AppCompatActivity(), CatalogContract.View {
         setContentView(binding.root)
         setSupportActionBar(toolbar)
 
-        presenter = CatalogPresenter().apply { attach(this@CatalogActivity) }
-        presenter.loadCatalog()
+        showCatalog()
 
         auth = FirebaseAuth.getInstance()
 
@@ -50,15 +50,13 @@ class CatalogActivity : AppCompatActivity(), CatalogContract.View {
 
         client = GoogleSignIn.getClient(this, gso)
 
-        router = CatalogRouter(this)
         binding.fabAdd.setOnClickListener {
-            router.goToEditorScreen()
+            goToEditorScreen()
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        presenter.detach()
+    private fun goToEditorScreen() {
+        Intent(this, EditorActivity::class.java).also { startActivity(it) }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -68,17 +66,17 @@ class CatalogActivity : AppCompatActivity(), CatalogContract.View {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.item_sign_out -> presenter.signOut()
-            R.id.item_rate -> presenter.goToPlayStore()
-            R.id.item_about -> presenter.goToAboutScreen()
+            R.id.item_sign_out -> showSignOutDialog()
+            R.id.item_rate -> goToPlayStore()
+            R.id.item_about -> goToAboutScreen()
         }
         return super.onOptionsItemSelected(item)
     }
 
-    override fun showCatalog(cuadres: MutableList<Cuadre>) {
+    fun showCatalog() {
         // TODO: Remove Toast after implementing Room database
         Toast.makeText(this, "WIP: Removed Firebase Database", Toast.LENGTH_LONG).show()
-        setupRecyclerView(cuadres)
+        setupRecyclerView(mutableListOf())
     }
 
     private fun setupRecyclerView(cuadres: MutableList<Cuadre>) {
@@ -97,7 +95,7 @@ class CatalogActivity : AppCompatActivity(), CatalogContract.View {
         }
     }
 
-    override fun showSignOutDialog() {
+    fun showSignOutDialog() {
         val dialog = setupSignOutDialog()
         dialog.show()
     }
@@ -114,16 +112,21 @@ class CatalogActivity : AppCompatActivity(), CatalogContract.View {
         setNegativeButton(android.R.string.cancel) { _: DialogInterface, _: Int -> }
     }
 
-    override fun rate() {
-        router.goToPlayStore()
+    private fun goToPlayStore() {
+        val url = "https://play.google.com/store/apps/details?id=com.chrisvasqm.cuadramo"
+        Intent(Intent.ACTION_VIEW)
+            .apply { data = Uri.parse(url) }
+            .also { startActivity(it) }
     }
 
-    override fun goToAboutScreen() {
-        router.goToAboutScreen()
+    private fun goToAboutScreen() {
+        Intent(this, AboutActivity::class.java).also { startActivity(it) }
     }
 
-    override fun goToSignInScreen() {
-        router.goToSignInScreen()
+    private fun goToSignInScreen() {
+        Intent(this, SignInActivity::class.java)
+            .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            .also { startActivity(it) }
     }
 
 
