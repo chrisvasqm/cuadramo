0d5bd19c803701da96aec27ad0642ebaf1da41b5
chrisvasqm <christianvasquezmelo@gmail.com>
2022-03-14 17:20:43 -0400

    Refactor SignInActivity

--- app/src/main/java/com/chrisvasqm/cuadramo/signin/SignInActivity.kt
+++ app/src/main/java/com/chrisvasqm/cuadramo/signin/SignInActivity.kt
@@ -11,18 +11,18 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import timber.log.Timber
 
-class SignInActivity : AppCompatActivity(), SignInContract.View {
+class SignInActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignInBinding
 
     private val TAG: String = this::class.java.simpleName
 
     private val RC_SIGN_IN: Int = 9001
-
-    private lateinit var presenter: SignInContract.Presenter
 
     private lateinit var client: GoogleSignInClient
 
@@ -31,61 +31,38 @@ class SignInActivity : AppCompatActivity(), SignInContract.View {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        presenter = SignInPresenter().apply { attach(this@SignInActivity) }
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build()
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
 
         client = GoogleSignIn.getClient(this, gso)
 
-        binding.btnSignIn.setOnClickListener { presenter.signIn() }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        presenter.detach()
+        binding.btnSignIn.setOnClickListener { signIn() }
     }
 
     override fun onStart() {
         super.onStart()
-
-        presenter.updateUi()
+        updateUi(FirebaseAuth.getInstance().currentUser)
     }
 
-    override fun signIn() {
+    private fun signIn() {
         val intent = client.signInIntent
         startActivityForResult(intent, RC_SIGN_IN)
     }
 
-    override fun updateUi(user: FirebaseUser?) {
+    private fun updateUi(user: FirebaseUser?) {
         val isLoggedIn = user != null
         if (isLoggedIn) goToCatalog()
     }
 
-    override fun goToCatalog() {
+    private fun goToCatalog() {
         val intent = Intent(this, CatalogActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
         }
         startActivity(intent)
         finish()
-    }
-
-    override fun logAccountId(id: String?) {
-        Timber.d(TAG, "authWithGoogle: $id")
-    }
-
-    override fun logLoginSuccessfully() {
-        Timber.d(TAG, "signInWithCredential: success")
-    }
-
-    override fun logLoginFailure(exception: Exception?) {
-        Timber.w(exception, "signInWithCredential:failure")
-    }
-
-    override fun showMessage(message: String) {
-        Snackbar.make(binding.signInLayout, message, Snackbar.LENGTH_SHORT).show()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -96,12 +73,27 @@ class SignInActivity : AppCompatActivity(), SignInContract.View {
 
             try {
                 val account = task.getResult(ApiException::class.java)
-                presenter.authWithGoogle(account)
+                val credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+                val auth = FirebaseAuth.getInstance()
+                auth.signInWithCredential(credential)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            updateUi(auth.currentUser)
+                        } else {
+                            showMessage("Authentication failed.")
+                            updateUi(null)
+                        }
+                    }
             } catch (exception: ApiException) {
                 Timber.w(TAG, "Google Sign In failed: $exception")
-                Snackbar.make(binding.signInLayout, "Google sign in failed.", Snackbar.LENGTH_LONG).show()
+                Snackbar.make(binding.signInLayout, "Google sign in failed.", Snackbar.LENGTH_LONG)
+                    .show()
             }
         }
+    }
+
+    private fun showMessage(message: String) {
+        Snackbar.make(binding.signInLayout, message, Snackbar.LENGTH_SHORT).show()
     }
 
 }
