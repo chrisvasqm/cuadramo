b4f5f6a67f548781089a8a28270c5ea5b9f84a12
chrisvasqm <christianvasquezmelo@gmail.com>
2022-03-16 17:26:55 -0400

    Remove duplicate model

--- app/src/main/java/com/chrisvasqm/cuadramo/data/CuadreRepository.kt
+++ app/src/main/java/com/chrisvasqm/cuadramo/data/CuadreRepository.kt
@@ -1,33 +1,31 @@
 package com.chrisvasqm.cuadramo.data
 
 import com.chrisvasqm.cuadramo.data.local.CuadreDao
-import com.chrisvasqm.cuadramo.data.local.toModel
 import com.chrisvasqm.cuadramo.data.model.Cuadre
-import com.chrisvasqm.cuadramo.data.model.toDomain
 import javax.inject.Inject
 
 class CuadreRepository @Inject constructor(
     private val dao: CuadreDao
 ) {
 
-    suspend fun all(): List<Cuadre> {
-        return dao.getAll().map { it.toModel() }
+    suspend fun all(): MutableList<Cuadre> {
+        return dao.getAll()
     }
 
     suspend fun find(id: Int): Cuadre {
-        return dao.find(id.toLong()).toModel()
+        return dao.find(id.toLong())
     }
 
-    suspend fun add(entity: Cuadre) {
-        dao.insert(entity.toDomain())
+    suspend fun add(cuadre: Cuadre) {
+        dao.insert(cuadre)
     }
 
-    suspend fun remove(entity: Cuadre) {
-        dao.delete(entity.toDomain())
+    suspend fun remove(cuadre: Cuadre) {
+        dao.delete(cuadre)
     }
 
-    suspend fun save(entity: Cuadre) {
-        dao.update(entity.toDomain())
+    suspend fun save(cuadre: Cuadre) {
+        dao.update(cuadre)
     }
 
     suspend fun exists(id: Int): Boolean {
