bd7d9f1f134be177a043212f22a09b289ce7b423
Christian Vasquez <christianvasquezmelo@gmail.com>
2022-03-25 20:57:19 -0400

    Remove tests
    These will be covered by that last one remaining

--- app/src/test/java/com/chrisvasqm/cuadramo/CuadreTests.kt
+++ app/src/test/java/com/chrisvasqm/cuadramo/CuadreTests.kt
@@ -17,51 +17,6 @@ class CuadreTests {
     }
 
     @Test
-    fun revenue_OneTicketSoldAndNoExpenses_ReturnsFifty() {
-        val cuadre = Cuadre(ticketsTotal = 2, ticketsLeft = 1)
-
-        val actual = cuadre.revenue
-
-        assertThat(actual).isEqualTo(50)
-    }
-
-    @Test
-    fun revenue_TwoTicketsSoldAndFiftyOnExpenses_ReturnsFifty() {
-        val cuadre = Cuadre(ticketsTotal = 2, ticketsLeft = 0, others = 50)
-
-        val actual = cuadre.revenue
-
-        assertThat(actual).isEqualTo(50)
-    }
-
-    @Test
-    fun revenue_TwoTicketsSoldAndOneFreebie_ReturnsFifty() {
-        val cuadre = Cuadre(ticketsTotal = 2, ticketsLeft = 0, freebies = 1)
-
-        val actual = cuadre.revenue
-
-        assertThat(actual).isEqualTo(50)
-    }
-
-    @Test
-    fun revenue_TwoTicketsSoldAndFoodCost_ReturnsFifty() {
-        val cuadre = Cuadre(ticketsTotal = 2, ticketsLeft = 0, food = 50)
-
-        val actual = cuadre.revenue
-
-        assertThat(actual).isEqualTo(50)
-    }
-
-    @Test
-    fun revenue_TwoTicketsSoldAndDelivery_ReturnsFifty() {
-        val cuadre = Cuadre(ticketsTotal = 2, ticketsLeft = 0, delivery = 50)
-
-        val actual = cuadre.revenue
-
-        assertThat(actual).isEqualTo(50)
-    }
-
-    @Test
     fun revenue_TenTicketsSoldWithOneFreebieAndFoodAndDeliveryAndOthers_ReturnsFifty() {
         val cuadre = Cuadre(
             ticketsTotal = 10,
